30c64f14f071e46c34c5376dc84d2d494afa4d1a
elin6 <elin6@uw.edu>
2021-04-06 17:19:51 -0700

    FINISHED

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Liz on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
